b10263110e863c73dc13ab1efde883c5398c64f8
Wendy Hintzen <wpah@comcast.net>
2020-11-21 12:34:42 -0500

    Added Navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -7,7 +7,7 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="/">
-        Pupster
+        KnoPressure
       </Link>
       <div>
         <ul className="navbar-nav">
@@ -15,28 +15,36 @@ function Navbar() {
             <Link
               to="/"
               className={
-                window.location.pathname === "/" || window.location.pathname === "/about"
+                window.location.pathname === "/" || window.location.pathname === "/landing"
                   ? "nav-link active"
                   : "nav-link"
               }
             >
+              Landing
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/login"
+              className={window.location.pathname === "/login" ? "nav-link active" : "nav-link"}
+            >
+              Login
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/home"
+              className={window.location.pathname === "/home" ? "nav-link active" : "nav-link"}
+            >
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/about"
+              className={window.location.pathname === "/about" ? "nav-link active" : "nav-link"}
+            >
               About
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/discover"
-              className={window.location.pathname === "/discover" ? "nav-link active" : "nav-link"}
-            >
-              Discover
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/search"
-              className={window.location.pathname === "/search" ? "nav-link active" : "nav-link"}
-            >
-              Search
             </Link>
           </li>
         </ul>
