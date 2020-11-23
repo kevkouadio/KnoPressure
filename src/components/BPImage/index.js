bb04c2099602169ee5086c19872196d076778394
Wendy Hintzen <wpah@comcast.net>
2020-11-23 17:52:40 -0500

    Updated BPImage index.js and style.css to create two columns

--- src/components/BPImage/index.js
+++ src/components/BPImage/index.js
@@ -1,17 +1,28 @@
 import React from "react";
 import Row from "../../components/Row";
 import Col from "../../components/Col";
+// import Container from "../../components/Container";
+import "./style.css";
 
 function BPImage () {
 return (
-<Row>
-    <Col size="col-lg-6">                                       
-        <p id="secondtext">Understanding Blood Pressure Readings</p> 
-             <img id="bloodpressure-chart" src={"./assets/images/blood pressure readings chart.jpg"} alt="bloodpressure-chart" 
-             className="img-fluid"/>                      
-                         
-    </Col>
-</Row>
+    
+  <section>      
+        <Row>
+            <Col size="md-6" id="bloodpressure">                                             
+                <p id="secondtext">Understanding Blood Pressure Readings</p> 
+                    <img id="bloodpressure-chart" src={"./assets/images/blood pressure readings chart.jpg"} alt="bloodpressure-chart" 
+                    className="img-fluid"/>                      
+                                
+            </Col>
+            <Col size="md-6" id="emptycol" >                                      
+                <p>Put Whatever you want here</p> 
+                                        
+                                
+            </Col>
+        </Row>
+</section>
+
 );
 
 }
