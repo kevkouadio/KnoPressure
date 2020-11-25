d9a3ca60f149e90453fe236898aa738ca0ab53c4
Wendy Hintzen <wpah@comcast.net>
2020-11-25 00:13:33 -0500

    Updated Searchform to align code

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -3,23 +3,20 @@ import Col from "../../components/Col";
 import "./style.css";
 
 function SearchForm() {
-return (
-  <Col size="md-6" id="emptycol" >                                      
-  <p>Kno pressure?! Let's find out where your numbers are today</p> 
-<form>
-<p>What are your numbers today? Let's find out if its normal</p>
-<label id="systolic-label" for="bp-systolic">Systolic/top number</label>
-<input type="text"></input>
-<br></br>
-<label id="diastolic-label" for="bp-systolic">Diastolic/bottom number</label>
-<input type="text"></input>
-<button>Submit</button>
-
-</form>
-</Col>
-);
-
-
+  return (
+    <Col size="md-6" id="emptycol" >                                      
+      <p>Kno pressure?! Let's find out where your numbers are today</p> 
+      <form>
+         <p>What are your numbers today? Let's find out if its normal</p>
+           <label id="systolic-label" for="bp-systolic">Systolic/top number</label>
+              <input type="text"></input>
+              <br></br>
+           <label id="diastolic-label" for="bp-systolic">Diastolic/bottom number</label>
+              <input type="text"></input>
+                 <button>Submit</button>
+      </form>
+    </Col>
+  );
 }
 
 export default SearchForm;
