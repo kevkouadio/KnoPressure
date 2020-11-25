780faf15c13dfa8d67e2d26bf89aa2b3a34fff29
Wendy Hintzen <wpah@comcast.net>
2020-11-25 00:03:37 -0500

    Created the About index.js and Aboutstyle.css and added some team images

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -2,62 +2,65 @@ import React from "react";
 import Row from "../../components/Row";
 import Container from "../../components/Container";
 import Col from "../../components/Col";
-import Navbar from "../../components/Navbar";
+import "./aboutstyle.css";
+
+function About() {
+    return (
+        <div> 
+           <Container style={{ marginTop: 30 }}>
+             <Row>
+                   <Col size="md-12">           
+                    <h1 className="display-5">About Us</h1>
+                    <hr />
+                    </Col>
+                    <Col size="md-10">  
+                        <div className="card border border-info">
+                            <div className="card-body">
+                                <p className="card-text">Welcome all to our KnoPressure app. Our App was built so that you can input your information in order to determine your blood pressure.
+                                High or Low blood pressure is such a deadly silent killer in our society today and many of us don't even know we have it.
+                                High blood pressure, also called hypertension, is when the pressure in your blood vessels is unusually high. It can be serious if not treated.
+                                Lifestyle changes like eating a healthy diet and exercising regularly can help lower high blood pressure. Some people also need to take medicines.
+                                Things that increase your chances of high blood pressure include being overweight, having an unhealthy diet, smoking and not exercising enough.
+                                </p>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col size="md-6">
+                        <div className="card-body">
+                            <h5>OUR TEAM</h5>
+                        </div>                   
+                    </Col>
+                     <Row>                                          
+                        <div className="card images">
+                            <img src={"./assets/images/Kev K.jpeg"} className="card-img-top" alt="Kevin" />
+                            <div className="card-body">
+                                <p className="card-text">Kouassi Kevin Kouadio </p>
+                            </div>
+                        </div>                       
+                        <div className="card images">
+                            <img src={"./assets/images/Wendy_Image.jpg"} className="card-img-top" alt="Wendy" />
+                                <div className="card-body">
+                                    <p className="card-text">Wendy Hintzen</p>
+                                </div>
+                        </div>                       
+                        <div className="card images">
+                            <img src={"./assets/images/tashena.jpeg"} className="card-img-top" alt="Tashena" />
+                                <div className="card-body">
+                                    <p className="card-text">Tashena 'Sheena' Malloy </p>
+                                </div>                        
+                        </div>                       
+                        <div className="card images">
+                            <img src={"./assets/images/miguel.png"} className="card-img-top" alt="Miguel" />
+                                <div className="card-body">
+                                    <p className="card-text">Miguel Lopez</p>
+                                </div>
+                        </div>                                           
+                    </Row>
+                </Row>  
+            </Container>     
+        </div>
+     );
+}
 
 
-function About(){
-        return (
-            <div>    
-              <Navbar />         
-              <Container style={{ marginTop: 30 }}>
-                <Row>
-                  <Col size="col-12">
-                    <h1>About KnoPressure</h1>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col size="col-12">
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquet diam tortor, id
-                      consequat mauris ullamcorper eu. Orci varius natoque penatibus et magnis dis
-                      parturient montes, nascetur ridiculus mus. Pellentesque et dui id justo finibus
-                      sollicitudin at et metus. Ut feugiat tellus nec metus commodo, sed suscipit nisi
-                      gravida. Duis eget vestibulum quam, ut porttitor sem. Donec sagittis mi sollicitudin
-                      turpis semper, et interdum risus lobortis. Vestibulum suscipit nunc non egestas
-                      tristique. Proin hendrerit efficitur malesuada. Mauris lorem urna, sodales accumsan
-                      quam non, tristique tempor erat. Nullam non sem facilisis, tempus tortor sit amet,
-                      volutpat nisl. Ut et turpis non nunc maximus mollis a vitae tortor. Pellentesque
-                      mattis risus ac quam laoreet cursus. Praesent suscipit orci neque, vestibulum
-                      tincidunt augue tincidunt non. Duis consequat mattis tortor vitae mattis.
-                    </p>
-                    <p>
-                      Phasellus at rutrum nisl. Praesent sed massa ut ipsum bibendum porttitor. Sed
-                      malesuada molestie velit ac viverra. Quisque a ullamcorper purus. Curabitur luctus mi
-                      ac mi hendrerit semper. Nulla tincidunt accumsan lobortis. Mauris convallis sapien non
-                      nibh porta accumsan. Nunc volutpat tempus porttitor. Nunc congue dictum egestas.
-                      Aliquam blandit mi eu urna scelerisque, vitae volutpat ligula ultricies. Maecenas vel
-                      porta augue. Fusce mauris ex, dignissim et lacinia ut, tempus eget nibh.
-                    </p>
-                    <p>
-                      Etiam ut massa efficitur, gravida sapien non, condimentum sapien. Suspendisse massa
-                      tortor, facilisis in neque sit amet, scelerisque elementum tortor. Nullam eget nibh
-                      sit amet odio lobortis ullamcorper. Nulla bibendum magna nec sem pulvinar lobortis.
-                      Mauris et imperdiet urna, vitae lobortis dui. Nunc elementum elit mi, non mattis enim
-                      congue at. Proin mi lectus, ullamcorper id hendrerit eu, ultricies vitae lacus. Nunc
-                      vehicula, erat eget laoreet condimentum, felis ante malesuada leo, nec efficitur diam
-                      nisi eget nisi. Cras arcu lacus, tristique in bibendum vitae, elementum eget lorem.
-                      Maecenas vestibulum volutpat orci eu pharetra. Praesent vel blandit ante, nec faucibus
-                      libero. Sed ultrices lorem ex, eu facilisis libero convallis ac. Vivamus id dapibus
-                      eros. Nullam tempor sem rhoncus porta semper. Proin bibendum vulputate nisl, fringilla
-                      interdum elit pulvinar eu. Quisque vitae quam dapibus, vestibulum mauris quis, laoreet
-                      massa.
-                    </p>
-                  </Col>
-                </Row>
-              </Container>
-            </div>
-          );
-        }
-
-
-export default About;
+export default About;  
