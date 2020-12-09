a10e65479c29d65003accd736d41ba846449ff34
Kouassi Kevin Kouadio <kevkouadio@gmil.com>
2020-12-09 12:42:22 -0500

    modification of chart

--- client/src/components/Chart/index.js
+++ client/src/components/Chart/index.js
@@ -29,7 +29,39 @@ function Chart() {
   });
   
   const myData = {
-    type: 'bar',
+    type: 'line',
+    labels: [
+    { //Scale to indicate value of normal bp rate 
+      text: 'Systolic/Normal',
+      hook: 'scale:name=scale-y,index=6',
+      backgroundColor: '#4caf50'
+    }, 
+    { 
+      text: 'Systolic/High BP',
+      hook: 'scale:name=scale-y,index=7',
+      backgroundColor: 'red'
+    }, 
+    { 
+      text: 'Diastolic/High BP',
+      hook: 'scale:name=scale-y,index=5',
+      backgroundColor: 'red'
+    },
+    {
+      text: 'Diastolic/Normal',
+      hook: 'scale:name=scale-y,index=4',
+      backgroundColor: '#4caf50'
+    }
+  ],
+  scaleY: {
+    label: {
+      text: "Systolic & Disatolic values"
+    },
+  },
+  scaleX: {
+    label: {
+      text: "Date & time"
+    }
+  },
     series: [
       { values: diastolicData, lineColor: 'red'},
       { values: systolicData, lineColor: 'blue'}
